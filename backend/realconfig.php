8b292bff804e40a131f748dcaae4288e613f20ab
804173948 <804173948@qq.com>
2019-04-16 00:50:27 +0800

    fix intro and time

--- backend/realconfig.php
+++ backend/realconfig.php
@@ -1,13 +1,1 @@
-<?php
-$ini = array('servername' => "localhost",
-	'username' => "root",
-	'password' => "",
-	'dbname' => "bbt");
-
-/*
-servername="localhost"
-username="root"
-password=""
-dbname="bbt"
-*/
-?>
+<?php $ini = array('servername' => "localhost",'username' => "root",'password' => "",'dbname' => "bbt");?>
